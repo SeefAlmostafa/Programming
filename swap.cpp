0a619b62e7895279fb976ed65d7822f9baa327f3
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-11-29 21:26:43 +0100

    add

---
+++ swap.cpp
@@ -0,0 +1,25 @@
+
+using namespace std;
+#include <iostream>
+
+void swap(int&, int&);
+
+int main()
+{
+	// Before
+	int x = 10, y = 20;
+
+	swap(x, y);
+
+	// After
+	cout << "x:" << x << endl; //20
+	cout << "y:" << y << endl; //10
+
+}
+
+void swap(int& x, int& y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
