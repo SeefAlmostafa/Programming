60c6431b6d2c3dba6aba5a9b32d52851caa8c36e
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-12 21:19:12 +0100

    update

--- vectors/main.cpp
+++ vectors/main.cpp
@@ -1,10 +1,11 @@
+
 /*
 	***********************************************************************
 	* choiseMode()	  : users can choise between (file,screen) as printmode
 	* exit_keys()     : exit/Exit to stop any insertion of data on a screen
 	* insert_data()   : The data can be inserted into a file or into screen
 	* set_size()	  : can be called to set the size of the vectors length
-	* print()		  : print could be called to diplay data  at the screen
+	* print()	: print could be called to diplay data  at the screen
 	* allowedSize()   : can be called to check if the vector size  is vaild
 	* get_name()      : get the name and the format of the nedded data file
 	***********************************************************************
