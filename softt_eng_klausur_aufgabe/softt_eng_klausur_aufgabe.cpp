66e685b1595fec50b3f968db6eaeb228d6c00921
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-22 04:15:25 +0100

    add klausur_frage

--- softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
+++ softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
@@ -1,8 +1,4 @@
 
-/*
-
-
-*/
 #include <iostream>
 using namespace std;
 
@@ -80,9 +76,10 @@ startkapital:9999,kundenPrio:1				           true			  	       true		   	 true		 
 
 /*
 Einfache bedingungueberdeckung:
-				startkapital < 10000	kundenPrio == 1	 startkapital > 50000	kundenPrio == 3
-s:9999,kp:3  	false					true		     true					false
-s:60000,kp:1	true				    false			 false	               	true
+											startkapital < 10000			kundenPrio == 1			startkapital > 50000			kundenPrio == 3
+startkapital:9999, kundenPrio:3				false						     true		            true							false
+startkapital:60000,kundenPrio:1				true						     false			        false	               			true
+
 
 
 Mehrfachbedingungueberdeckung:
