ea5a3c365455927b1b0b36147d3a3e4635108b5a
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-22 03:53:34 +0100

    1

--- softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
+++ softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
@@ -41,26 +41,66 @@ int main() {
 	
 }
 
+
+
 /*
-* Einfache bedingungueberdeckung:
+
+Einfache bedingungueberdeckung:
+							   startkapital < 10000      		 kundenPrio == 1	      		     startkapital > 50000	         kundenPrio == 3
+startkapital:9999, kundenPrio:3			                   true				      false			      		     false	               	     true
+startkapital:60000,kundenPrio:1				           false			      true		             		     true	                     false
+
+
+
+
+
+
+Mehrfachbedingungueberdeckung:
+								startkapital < 10000      		  kundenPrio == 1	       		      startkapital > 50000	   	 kundenPrio == 3
+startkapital:9999, kundenPrio:3			                   true			 	      false			     		     false	                     true
+startkapital:60000,kundenPrio:1				           false		              true		             		     true	                     false
+startkapital:9999, kundenPrio:1				           true			              true			      		     false		             false
+startkapital:60000,kundenPrio:3				           false			      false			     		     true		             true
+
+
+
+
+
+
+Minmale mehrfachbedingungueberdeckung
+								startkapital < 10000        	   kundenPrio == 1	   	  E1	      startkapital > 50000	 	  kundenPrio == 3      	 E2
+startkapital:9999, kundenPrio:3			                   true			 	       false	   		false		     false     		      	      true		true
+startkapital:9999,kundenPrio:1				           true			  	       true		   	 true		     false		 	      false		false
+
+*/
+
+
+
+
+
+
+
+
+
+/*
+Einfache bedingungueberdeckung:
 											startkapital < 10000			kundenPrio == 1			startkapital > 50000			kundenPrio == 3
 startkapital:9999, kundenPrio:3				false						     true		            true							false
 startkapital:60000,kundenPrio:1				true						     false			        false	               			true
-* 
-* 
-* 
-* Mehrfachbedingungueberdeckung:
+
+
+
+Mehrfachbedingungueberdeckung:
 
 											startkapital < 10000			kundenPrio == 1			startkapital > 50000			kundenPrio == 3
 startkapital:9999, kundenPrio:3				false						     true		            true							false
 startkapital:60000,kundenPrio:1				true						     false			      	false	               			true
 startkapital:9999, kundenPrio:1				true			                 true			      	false							false
 startkapital:60000,kundenPrio:3				false							 false					true							true
-* 
-* 
-* 
-* 
-* Minmale mehrfachbedingungueberdeckung
+
+
+
+Minmale mehrfachbedingungueberdeckung
 
 
 											startkapital < 10000			kundenPrio == 1			E1			startkapital > 50000			kundenPrio == 3			E2
