9eaad6d3ab75f1ef9097f2923a94f494387a5c07
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-22 04:09:34 +0100

    d

--- softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
+++ softt_eng_klausur_aufgabe/softt_eng_klausur_aufgabe.cpp
@@ -1,28 +1,6 @@
 
 /*
-Einfache bedingungueberdeckung:
-                                   startkapital < 10000   kundenPrio == 1    startkapital > 50000    kundenPrio == 3
-startkapital:9999, kundenPrio:3    false				  true		         true					 false
-startkapital:60000,kundenPrio:1    true					  false			     false	                 true
-
-
-
-Mehrfachbedingungueberdeckung:
-
-											startkapital < 10000			kundenPrio == 1			startkapital > 50000			kundenPrio == 3
-startkapital:9999, kundenPrio:3				false						     true		            true							false
-startkapital:60000,kundenPrio:1				true						     false			      	false	               			true
-startkapital:9999, kundenPrio:1				true			                 true			      	false							false
-startkapital:60000,kundenPrio:3				false							 false					true							true
-
-
-
-Minmale mehrfachbedingungueberdeckung
-
-
-											startkapital < 10000			kundenPrio == 1			E1			startkapital > 50000			kundenPrio == 3			E2
-startkapital:9999, kundenPrio:3				true			 				false	   				false		false     						true					true
-startkapital:9999,kundenPrio:1				true			  			    true					true		false		 					false					false
+My name is saif an i am 20												years old
 
 */
 #include <iostream>
