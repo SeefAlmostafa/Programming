4fb22acec1de6b27f88ecf025d434395eb97ab98
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-23 01:58:46 +0100

    s

--- soft_eng_/soft_eng_.cpp
+++ soft_eng_/soft_eng_.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 
 /*
-Einfache bedingungueberdeckung:
-									startkapital < 10000                    kundenPrio == 1         startkapital > 50000            kundenPrio == 3
-startkapital:9999, kundenPrio:3		true                                    false                   false                           true
-startkapital:60000,kundenPrio:1		false                                   true                    true                            false
+Minmale mehrfachbedingungueberdeckung:
+startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
+true                            false                   false   false                   false                   false
+true                            true                    true    false                   false                   false
+
 
 
 */
