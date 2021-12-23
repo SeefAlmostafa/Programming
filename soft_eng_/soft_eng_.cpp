f46d68861eb16ca26ca3b8954f643b5fdbdd527b
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-23 02:04:44 +0100

    s

--- soft_eng_/soft_eng_.cpp
+++ soft_eng_/soft_eng_.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 
 /*
-Minmale mehrfachbedingungueberdeckung:
-startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
-true                            false                   false   false                   false                   false
-true                            true                    true    false                   false                   false
+
 
 
 
@@ -50,11 +47,13 @@ int main() {
 
 
 	string t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-	t1 = "Minmale mehrfachbedingungueberdeckung:\n";
-	t2 = "startkapital < 10000\t\tkundenPrio == 1\t\tE1\tstartkapital > 50000\tkundenPrio == 3\t\tE2\n";
-	t5 = "true\t\t\t\tfalse\t\t\tfalse\tfalse\t\t\tfalse\t\t\tfalse\n";
-	t6 = "true\t\t\t\ttrue\t\t\ttrue\tfalse\t\t\tfalse\t\t\tfalse\n";
-	cout << t1 << t2  << t5 << t6;
+	t1 = "Mehrfachbedingungueberdeckung:\n";
+	t2 = "startkapital < 10000\t\tkundenPrio == 1\t\tstartkapital > 50000\tkundenPrio == 3\t\n";
+	t5 = "true\t\t\t\tfalse\t\t\tfalse\t\t\ttrue\t\t\n";
+	t6 = "true\t\t\t\ttrue\t\t\tfalse\t\t\tfalse\t\t\n";
+	t7 = "true\t\t\t\tfalse\t\t\tfalse\t\t\tfalse\t\t\n";
+	t8 = "true\t\t\t\ttrue\t\t\tfalse\t\t\tfalse\t\t\n";
+	cout << t1 << t2  << t5 << t6<<t7<<t8;
 }
 
 /*
@@ -69,7 +68,13 @@ Minmale mehrfachbedingungueberdeckung:
                                		startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
 startkapital:60000,kundenPrio:1		true                            false                   false   false                   false                   false
 startkapital:9999,kundenPrio:3 		true                            true                    true    false                   false                   false
-	
+
+Mehrfachbedingungueberdeckung:
+									startkapital < 10000            kundenPrio == 1         startkapital > 50000    kundenPrio == 3
+									true                            false                   false                   true
+									true                            true                    false                   false
+									true                            false                   false                   false
+									true                            true                    false                   false
 
 */
 
