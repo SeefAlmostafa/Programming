464f1c90a2d6d33186c168842055f4dd4c8bba7b
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-23 01:42:22 +0100

    d

--- soft_eng_/soft_eng_.cpp
+++ soft_eng_/soft_eng_.cpp
@@ -53,8 +53,8 @@ startkapital:9999, kundenPrio:3		true                                    false  
 startkapital:60000,kundenPrio:1		false                                   true                    true                            false
 
 Minmale mehrfachbedingungueberdeckung:
-									startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
-									true                            false                   false   false                   false                   false
+startkapital:9999,kundenPrio:3		startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
+startkapital:60000,kundenPrio:1		true                            false                   false   false                   false                   false
 									true                            true                    true    false                   false                   false
 	
 */
