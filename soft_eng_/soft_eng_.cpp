b89b636df25134b68a0fc765c0fa197c07aec7a5
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-23 03:24:32 +0100

    updated

--- soft_eng_/soft_eng_.cpp
+++ soft_eng_/soft_eng_.cpp
@@ -34,51 +34,28 @@ int main() {
 
 	float endkapital = bestimmeEndkapital(startkapital, kundenPrio);
 	cout << "endkapital:" << endkapital << endl;
-
-
-	/*
-	string t1, t2, t3, t4, t5, t6, t7, t8, t9, t10;
-	t1 = "Mehrfachbedingungueberdeckung:\n";
-	t2 = "startkapital < 10000\t\tkundenPrio == 1\t\tstartkapital > 50000\tkundenPrio == 3\t\n";
-	t5 = "true\t\t\t\tfalse\t\t\tfalse\t\t\ttrue\t\t\n";
-	t6 = "false\t\t\t\ttrue\t\t\ttrue\t\t\tfalse\t\t\n";
-	t7 = "true\t\t\t\ttrue\t\t\tfalse\t\t\tfalse\t\t\n";
-	t8 = "false\t\t\t\tfalse\t\t\ttrue\t\t\ttrue\t\t\n";
-	cout << t1 << t2  << t5 << t6<<t7<<t8;
-	*/
 }
 
 /*
 
 Einfache bedingungueberdeckung:
-                                	startkapital < 10000                    kundenPrio == 1         startkapital > 50000            kundenPrio == 3	
-startkapital:9999, kundenPrio:3		true                                    false                   false                           true
-startkapital:60000,kundenPrio:1		false                                   true                    true                            false
+                                		startkapital < 10000		kundenPrio == 1								startkapital > 50000						kundenPrio == 3	
+startkapital:9999, kundenPrio:3			true						 false										false										true
+startkapital:60000,kundenPrio:1			false						 true										true										false
+
+
+Mehrfachbedingungueberdeckung:
+										startkapital < 10000		kundenPrio == 1								startkapital > 50000						kundenPrio == 3
+startkapital:9999,kundenPrio:3			true						 false										false										true
+startkapital:60000,kundenPrio:1			false						 true										true										false
+startkapital:9999,kundenPrio:1			true						 true										false										false
+startkapital:60000,kundenPrio:3			false						 false										true										true
 
 
 
 Minmale mehrfachbedingungueberdeckung:
-                               		startkapital < 10000            kundenPrio == 1         E1      startkapital > 50000    kundenPrio == 3         E2
-startkapital:9999,kundenPrio:1		true                            true					true	false                   false                   false
-startkapital:9999,kundenPrio:3 		true                            false                   false   false                   true					true
-
-
-
-Mehrfachbedingungueberdeckung:
-									startkapital < 10000              kundenPrio == 1         startkapital > 50000       	  kundenPrio == 3
-startkapital:9999,kundenPrio:3		true                              false                   false                           true
-startkapital:60000,kundenPrio:1		false                             true                    true                            false
-startkapital:9999,kundenPrio:1		true							  true                    false                           false
-startkapital:60000,kundenPrio:3		false                             false                   true		                      true
-
-
+										startkapital < 10000		kundenPrio == 1			E1					startkapital > 50000						kundenPrio == 3			E2
+startkapital:9999,kundenPrio:1			true						 true					true				false										false					false
+startkapital:9999,kundenPrio:3 			true						 false					false				false										true					true
 */
 
-/*
-Mehrfachbedingungueberdeckung:
-								startkapital < 10000            kundenPrio == 1         startkapital > 50000    kundenPrio == 3
-startkapital:9999,kundenPrio:3					true                            false                   false                   true
-startkapital:60000,kundenPrio:1					false                           true                    true                    false
-startkapital:9999,kundenPrio:1					true                            true                    false                   false
-startkapital:60000,kundenPrio:3					false                           false                   true                    true
-*/
