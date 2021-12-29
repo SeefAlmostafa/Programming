ca04539fa25bf36208cccb7c6cc1d8113bb209a2
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2021-12-29 03:05:04 +0100

    add initializer list

---
+++ initializer list/initializer list.cpp
@@ -0,0 +1,52 @@
+using namespace std;
+#include <iostream>
+
+class A {
+public:
+	A() { cout << "A Constructor" << endl; }
+	~A(){ cout << "A Destructor" << endl; }
+};
+
+
+class B {
+public:
+	B() { cout << "B Constructor" << endl; }
+	~B(){ cout << "B Destructor" << endl; }
+};
+
+
+class C {
+public:
+	C() { cout << "C Constructor" << endl; }
+	~C(){ cout << "C Destructor" << endl; }
+};
+
+
+class D {
+private:
+	A a;
+	B b;
+	C c;
+public:
+	D() :b(B()), a(A()), c(C()) { cout << "D Constructor\n"; }
+
+	~D() { cout << "D Destructor" << endl; }
+};
+
+
+int main() {
+	D d;
+
+	return 0;
+}
+
+/*
+A Constructor
+B Constructor
+C Constructor
+D Constructor
+D Destructor
+C Destructor
+B Destructor
+A Destructor
+*/
