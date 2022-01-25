1c97a5eb2bcb978004dd0177c77d063605bbce76
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2022-01-25 16:36:00 +0100

    add klausur

---
+++ klausur_programmierung3_fragen/klausur_programmierung3_fragen.cpp
@@ -0,0 +1,37 @@
+
+using namespace std;
+#include <iostream>
+
+
+// klasse in der KLausur
+class anything {
+private:
+	char c;
+	char* xp;
+	char* pp;
+public:
+	anything(char c );
+	anything();
+	~anything();
+};
+
+
+int main(){
+/*
+// klausurFragen:
+// abgeleitete und BasisKlasse aus einer der alten klausren ist noch eimnal vorgekommen
+// ist protectted das höchste Zugreigbar-stufe in c++ ?
+// Klasse ist gegeben und man muss Konstruktor aufrufe ordnen !
+// virtual Fragen
+
+
+// Frage 1: ist anything::anything(char c):c(0) Richtig/Flasch mit Begründung
+// Frage 2: ist anything::~anything(char c){} Rightig/ Falsch  mit Begründung
+// Frage 3: ist anything::~anything(){delete c;} Rightig/ Falsch  mit Begründung
+// Frage 4: ist anything::anything(char c){c = 1;  xp = &c; } Richtig/Flasch mit Begründung
+
+*/
+
+}
+
+
