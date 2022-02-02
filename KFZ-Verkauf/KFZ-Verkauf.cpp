d4e9b35d3c6b9603bbf75ddd350450e7da67e33e
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2022-02-02 04:31:37 +0100

    updated

--- KFZ-Verkauf/KFZ-Verkauf.cpp
+++ KFZ-Verkauf/KFZ-Verkauf.cpp
@@ -2,31 +2,32 @@
 using namespace std;
 #include <iostream>
 
+// Aufgabe (a)
+// Uebung 12 Software Eng
+
 double calculate_price(double baseprice, double specialprice, double extraprice, int extras, double discount);
 double calculate_price(double baseprice, double specialprice, double extraprice, int extras, double discount = 0.0) {
 	
-	double cashDiscount = 0.0, Zubehoerrabatt = 0.0;
 
 	if (baseprice <= 0.0 || specialprice < 0.0 || extraprice < 0.0 || discount < 0.0) {
 		baseprice = 0.0;	discount = 0.0;	specialprice = 0.0;	extraprice = 0.0;
 	}
 
-	if (extras > 0 && extras < 5) {
+	if (extras == 3 || extras == 4) {
 		discount = 10.0;
-		cashDiscount = (discount * baseprice) / 100;
-		Zubehoerrabatt = (extraprice * discount) / 100;
 	}
 
 	else if(extras >= 5) {
 		discount = 15.0;
-		cashDiscount = (discount * baseprice) / 100;
-		Zubehoerrabatt = (extraprice * discount) / 100;
 	}
 
 	else{
-		 cout << "extras kann nicht kleiner oder gleich 0 sein !" << endl;
 		 discount = 0.0;
 	 }
+
+	double cashDiscount = 0.0, Zubehoerrabatt = 0.0;
+	cashDiscount = (discount * baseprice) / 100;
+	Zubehoerrabatt = (extraprice * discount) / 100;
 
 	return (((baseprice - cashDiscount) + specialprice + extraprice)) - Zubehoerrabatt;
 }
@@ -41,3 +42,8 @@ int main(){
 	cout << GesamtPreis << endl;
 	return 0;
 }
+
+
+
+// Aufgabe (b)
+// 
