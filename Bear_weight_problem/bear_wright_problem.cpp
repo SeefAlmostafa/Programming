96db2c70c1d2aa38da40df1d8cb56a71db386c7d
SeefAlmostafa <83471425+SeefAlmostafa@users.noreply.github.com>
2022-02-15 04:21:31 +0100

    Add Bear_Puzzle

---
+++ Bear_weight_problem/bear_wright_problem.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+    int res = 0; // first year
+    int s,b;     // s = sam's weight, b = weight of Bob
+    cout << "Enter the weight of sam:";
+    cin >> s;
+    cout << "Enter the weight of Bob:";
+    cin >> b;
+
+    while(s <= b){ // as long sam's wight <=  Bob's weight
+        s *= 3;    // then keep counting years
+        b *= 2;                     
+        ++res;
+    }
+    cout <<"Years:"<< res << endl; 
+    return 0;
+}
+
+/*
+Test case 1:
+Enter the weight of sam:4
+Enter the weight of Bob:7
+Years:2
+
+Test case 2:
+Enter the weight of sam:4
+Enter the weight of Bob:9
+Years:3
+
+Test case 3:
+Enter the weight of sam:1
+Enter the weight of Bob:1
+Years:1
+*/
