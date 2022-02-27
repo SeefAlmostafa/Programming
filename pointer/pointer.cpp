4e19d1983f064461fe024c8d5a13b51b01245331
Seef <seef.almustafa@gmail.com>
2022-02-27 16:22:10 +0100

    Add pointer

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -1,14 +1,40 @@
 using namespace std;
 #include <iostream>
 
-int sum(int n) {
-    if (n == 0) {
-        return n;
-    }
-    return n + sum(n - 1);
-}
-
 int main() {
-    int res = sum(5);
-    cout << res << endl;
+    int ***ptr = new int **[3];  // 3D Array
+
+    for (int i(0); i < 3; i++) {
+        ptr[i] = new int *[3];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j(0); j < 3; j++)
+            ptr[i][j] = new int[3];
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j(0); j < 3; j++)
+            for (int k(0); k < 3; k++)
+                ptr[i][j][k] = k + 1;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j(0); j < 3; j++)
+            for (int k(0); k < 3; k++)
+                cout << "ptr[" << i << "][" << j << "][" << k << "]: " << ptr[i][j][k] << endl;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j(0); j < 3; j++) {
+            delete[] ptr[i][j];
+            ptr[i][j] = nullptr;
+        }
+        delete[] ptr[i];
+        ptr[i] = nullptr;
+    }
+    delete[] ptr;
+    ptr = nullptr;
+
+    return 0;
 }
