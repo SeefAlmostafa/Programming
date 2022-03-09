4dd1c4ba6b16ac9f64461323f0d11befe2c62b51
Seef <seef.almustafa@gmail.com>
2022-03-09 23:30:33 +0100

    update array_problem

--- array_problem/array_problem.cpp
+++ array_problem/array_problem.cpp
@@ -1,44 +1,40 @@
 
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 
 void findTriplets(int arr[], int n) {
-	bool found = false;
-	for (int i = 0; i < n - 2; i++) {
-		for (int j = i + 1; j < n - 1; j++) {
-			for (int k = j + 1; k < n; k++) {
-				if (arr[i] + arr[j] + arr[k] == 0) {
-					cout <<
-						arr[i] << " " <<
-						arr[j] << " " <<
-						arr[k] << " " << endl;
-					found = true;
-				}
-			}
-		}
-	}
-	if (found == false) {
-		cout << " not exist " << endl;
-	}
+    bool found = false;
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (arr[i] + arr[j] + arr[k] == 0) {
+                    cout << arr[i] << " " << arr[j] << " " << arr[k] << " " << endl;
+                    found = true;
+                }
+            }
+        }
+    }
+    if (found == false) {
+        cout << " not exist " << endl;
+    }
 }
 
 int main() {
+    // A simple C++ program to find three elements
+    // whose sum is equal to zero
 
-	// A simple C++ program to find three elements
-	// whose sum is equal to zero
+    int arr[5] = {0, -1, 2, -3, 1};  // given array
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-	int arr[5] = { 0, -1, 2, -3, 1 }; // given array
-	int n = sizeof(arr) / sizeof(arr[0]);
+    findTriplets(arr, n);
 
-	findTriplets(arr, n);
-
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
 
 /*
-	Output:
-	0 - 1 1
-	2 - 3 1
+        Output:
+        0 - 1 1
+        2 - 3 1
 */
