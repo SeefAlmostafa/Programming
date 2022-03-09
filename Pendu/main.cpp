ee313a78ed9a4fcbbd518b72a4cfce2bbc256468
Seef <seef.almustafa@gmail.com>
2022-03-09 23:28:57 +0100

    Update Game

--- Pendu/main.cpp
+++ Pendu/main.cpp
@@ -1,4 +1,6 @@
 using namespace std;
+#include <conio.h>
+
 #include <iostream>
 
 #include "Pendu.h"
@@ -6,5 +8,19 @@ using namespace std;
 int main() {
     Pendu p;
     p.Run();
+
+    bool keepPlaying = true;
+    int character;
+
+    while (keepPlaying) {
+        cout << "\nPress Y to play again ...";
+        character = _getche();
+        if (character == 'Y' || character == 'y') {
+            p.Run();
+        } else {
+            cout << "The game was exited successfully" << endl;
+            keepPlaying = false;
+        }
+    }
     return 0;
 }
