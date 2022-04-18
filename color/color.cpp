83bd80711ece662f3adc02ef4e64b689324d3199
Seef <seef.almustafa@gmail.com>
2022-04-18 14:33:41 +0200

    add colors

---
+++ color/color.cpp
@@ -0,0 +1,14 @@
+﻿using namespace std;
+#include <windows.h>
+
+#include <iostream>
+
+int main() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    for (int k = 1; k < 16; k++) {
+        SetConsoleTextAttribute(hConsole, k);
+        cout << k << "- C++ is a fun language" << endl;
+    }
+    cout << "\n";
+    system("pause");
+}
