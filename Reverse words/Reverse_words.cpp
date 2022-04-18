9674e41b43d23ed8e9530a080189747063ee666d
Seef <seef.almustafa@gmail.com>
2022-04-18 14:26:48 +0200

    Add reverse_word

---
+++ Reverse words/Reverse_words.cpp
@@ -0,0 +1,18 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+string reverse_words(string str) {
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++) {
+        swap(str[i], str[n - i - 1]);
+    }
+    return str;
+}
+
+int main() {
+    cout << reverse_words("programming");
+    cout << "\n";
+    system("pause");
+    return 0;
+}
