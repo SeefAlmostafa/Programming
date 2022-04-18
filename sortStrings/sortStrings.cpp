9841283e3ed83e0f38f7852d60e2df1459cf8653
Seef <seef.almustafa@gmail.com>
2022-04-18 14:27:39 +0200

    add sort string

---
+++ sortStrings/sortStrings.cpp
@@ -0,0 +1,35 @@
+﻿
+using namespace std;
+#include <cstring>
+#include <iostream>
+#include <string>
+#define n 5
+
+void sortStrings(string names[]) {
+    string temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (strcmp(names[j].c_str(), names[j + 1].c_str()) > 0) {
+                temp = names[j];
+                names[j] = names[j + 1];
+                names[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    string names[n];
+    cout << "Enter the names:" << endl;
+
+    for (int i = 0; i < n; i++)
+        getline(cin, names[i]);
+
+    sortStrings(names);
+
+    for (int i = 0; i < n; i++)
+        cout << names[i] << " " << endl;
+
+    system("pause");
+    return 0;
+}
